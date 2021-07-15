a68e01a7852ab08650a57426c2a74fc6d772e901
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-07-15 09:28:47 -0700

    Adding db functions

--- fake_db/Db.js
+++ fake_db/Db.js
@@ -1,8 +1,20 @@
 class Db{
     constructor(){
-        this.users = {}      //will contain "Username: Total Points"
+        this.payers = {}      //will contain "Username: Total Points"
         this.transactions = []   //will be used to store transaction history (maybe use heap instead?)
                             //bit confused, if subtracting brings it negative, should transaction be removed
+    }
+
+    //assume transaction object will be {payer: "", points: "", timestamp: ""}
+    add_transaction(transaction){
+        if(transaction.payer in this.payers){
+            this.payers[transaction.payer] += transaction.points;   //Might need to convert to int from str
+        }
+        this.transactions.push(transaction)
+    }
+
+    all_balances(){
+        return this.payers;
     }
 
     print_test(){
