23365a292907a2f9167e8df8cff7449ff69e84b7
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-07-14 23:36:44 -0700

    Added 404 catch for bad api requests

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # fetch_points
+
+Run "node app.js" to open server
+To access, send api calls to localhost:5000 while app is running
--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,10 @@ const router = express.Router();
 
 router.use('/api/points', pointsRouter);
 
+//Add error catcher for bad requests
+router.use((req, res) => {
+    console.log("Test")
+    res.sendStatus(404);
+})
+
 module.exports = router;
