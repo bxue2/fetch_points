1cfc1d4bf792ea27bf0df296c56d341276d47bb1
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-07-14 23:58:38 -0700

    Adding the fake Db class

---
+++ fake_db/Db.js
@@ -0,0 +1,12 @@
+class Db{
+    constructor(){
+        this.users = {}      //will contain "Username: Total Points"
+        this.transactions = []   //will be used to store transaction history (maybe use heap instead?)
+                            //bit confused, if subtracting brings it negative, should transaction be removed
+
+    }
+}
+
+let fake_db = new Db()
+
+module.exports = fake_db;
