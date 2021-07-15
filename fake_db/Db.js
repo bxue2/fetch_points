65fe184f52908ce6c3c4e57dc5aa76a9df6a6c3a
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-07-15 00:02:26 -0700

    Linking fake_db to api route, testing

--- fake_db/Db.js
+++ fake_db/Db.js
@@ -3,7 +3,10 @@ class Db{
         this.users = {}      //will contain "Username: Total Points"
         this.transactions = []   //will be used to store transaction history (maybe use heap instead?)
                             //bit confused, if subtracting brings it negative, should transaction be removed
+    }
 
+    print_test(){
+        console.log("Hello")
     }
 }
 
--- routes/api/points.js
+++ routes/api/points.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const
+const fake_db = require('../../fake_db/Db')
+
+router.get("/", (req, res) => {
+    fake_db.print_test();
+    return res.status(200).send({
+        message: 'Printed test route'
+    })
+})
 
 module.exports = router;
